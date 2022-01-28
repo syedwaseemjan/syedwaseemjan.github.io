ef24a200865e5d2f85cd1b6594aca823075905c5
Waseem Jan <waseem.jan@britecore.com>
2022-01-29 01:07:43 +0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+var MainJS = {
+
+  bigImgEl : null,
+  numImgs : null,
+
+  init : function() {
+    // Shorten the navbar after scrolling a little bit down
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar").addClass("top-nav-short");
+        } else {
+            $(".navbar").removeClass("top-nav-short");
+        }
+    });
+
+    // On mobile, hide the avatar when expanding the navbar menu
+    $('#main-navbar').on('show.bs.collapse', function () {
+      $(".navbar").addClass("top-nav-expanded");
+    });
+    $('#main-navbar').on('hidden.bs.collapse', function () {
+      $(".navbar").removeClass("top-nav-expanded");
+    });
+  }
+};
+
+document.addEventListener('DOMContentLoaded', MainJS.init);
